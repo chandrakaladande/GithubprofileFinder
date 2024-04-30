97cfcf0f9a249208f8a8ff02a824a06f664048bb
chandrakala918 <chandrakaladande918@gmail.com>
2024-04-30 12:49:09 +0530

    Added

---
+++ app-github-profilefinder/src/App.js
@@ -0,0 +1,13 @@
+ import './App.css';
+import GithubProfileFinder from './Components/GithubProfileFinder';
+
+function App() {
+  return (
+    <div className="App">
+     Github profile finder
+       <GithubProfileFinder/>
+    </div>
+  );
+}
+
+export default App;
